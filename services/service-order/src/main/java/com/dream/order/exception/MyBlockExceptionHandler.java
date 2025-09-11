a37403a248945bc70b936110f4573814ec5e8d73
huzejun <961639130@qq.com>
2025-09-11 12:43:09 +0800

    sentinel 异常处理 web接口 2025-9-11 12:42:59

---
+++ services/service-order/src/main/java/com/dream/order/exception/MyBlockExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.dream.order.exception;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc_v6x.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.dream.common.R;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.PrintWriter;
+
+/**
+ * @Author huzejun
+ * @Date 2025-09-11 12:25
+ **/
+
+@Component
+public class MyBlockExceptionHandler implements BlockExceptionHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       String resourceName, BlockException e) throws Exception {
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+//        response.setContentType("application/json;charset=utf-8");  //出现中文乱码
+
+        R error = R.error(500, resourceName + "被Sentinel限制了，原因：" + e.getClass());
+
+        String json = objectMapper.writeValueAsString(error);
+        writer.write(json);
+
+        writer.flush();
+        writer.close();
+    }
+}
